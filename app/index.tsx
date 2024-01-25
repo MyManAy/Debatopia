4de178e76b6b5ca448895c06faaba7cf5de3b299
MyManAy <66086069+MyManAy@users.noreply.github.com>
2024-01-24 22:30:59 -0600

    removed unused styles

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,5 @@
 import { Redirect } from "expo-router";
-import { StyleSheet } from "react-native";
 
 export default function TabOneScreen() {
   return <Redirect href={"/topicList/"} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    alignItems: "center",
-  },
-});
